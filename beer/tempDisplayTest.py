67e4840d73869305d116fe18cf8d4f524c2d0d3e
Obvious-Moose <kenneth.c.michaels@gmail.com>
2016-03-06 12:52:45 -0500

    Update tempDisplayTest.py
    Adding the relay test at a couple temps.  Will need a better way to adjust the temps other than rewriting the code, but I can deal with that later.

--- beer/tempDisplayTest.py
+++ beer/tempDisplayTest.py
@@ -1,24 +1,25 @@
 import time
 from Adafruit_LED_Backpack import AlphaNum4
-#I guess I got the tempProbe class form Adafruit.  Don't really remember, but I sure didn't write it myself
+#I guess I got the tempProbe class form Adafruit.  Don't really remember, but I sure as hell didn't write it myself
 import tempProbe
 
 #This is for the relay stuff, which probably won't work anyway so who cares.
-#import RPi.GPIO as GPIO
-#GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
 #Create an instance of the 14-segment LED Backpack thing, and also the temperature probe dealie
 display = AlphaNum4.AlphaNum4()
 probe = tempProbe.tempProbe()
 
 #Have to run the display thing once, and also going to clean up the GPIO crap.  Makes sure it's set to off initially.
-#I think 22 is the bottom plug and 27 is the top plug.  Only need one for now; no heater.  Ignoring 27 I guess.
+#It's a 4-channel relay, but I only need two channels.  Wired to 14, 15, 18, and 23; only going to use 18 and 23.  
+#GPIO 18 should be the bottom plug; 23 should be the top.  Went ahead and unplugged 14 and 15 from the relay board.
 display.begin()
-#GPIO.cleanup()
-#GPIO.setup(22, GPIO.OUT)
-#GPIO.setup(27, GPIO.OUT)
-#GPIO.output(22, GPIO.LOW)
-#GPIO.output(27, GPIO.LOW)
+GPIO.cleanup()
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.output(18, GPIO.LOW)
+GPIO.output(23, GPIO.LOW)
 
 #Variables I might use below
 tempF = 0
@@ -44,14 +45,17 @@ while True:
   
   #This part actually controls the temperature?  It SHOULD flip the relay and turn the cooler on if the beer temp
   #   gets above 74.  Again, I doubt this even works who cares.
-  #if tempF > 74:
-  #  GPIO.output(22, GPIO.HIGH)
-  #elif tempF < 70:
-  #  GPIO.output(22, GPIO.LOW)
+  if tempF > 85:
+    GPIO.output(18, GPIO.HIGH)
+  elif tempF < 85 & tempF > 60:
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+  elif tempF < 60:
+    GPIO.output(23, GPIO.HIGH)
   
   #Ideally, I'd want to set this to check multiple times.  One reading of 74 could be a fluke (especially given
   #how shitty the probe was when i tested it.)  I'd want it to stay off until it gets 6-10 readings above 74. 
   #Similarly, it shouldn't kick off until it reads below 70 several times.  Doubt what I have works anyway so who cares.
   
   #Pause before the loop.  No need to update everything all that frequently.
-  time.sleep(1)
+  time.sleep(2)
